fca38ebf2022e670639d3313f951b5c8cfecb0c9
phusinhngay2011 <pminimalism2011@gmail.com>
2024-07-16 21:28:49 +0700

    update

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,4 @@
+
 // Path to the JSON file containing image filenames
 const imagesJsonPath = "assets/images/Bones/images.json";
 
@@ -218,6 +219,65 @@ function resetEvaluations() {
 //     return "Bác sĩ vui lòng tải lại file trước khi tắt trình duyệt";
 // };
 
+function imageZoom(imgID, resultID) {
+    var img, lens, result, cx, cy;
+    
+    result = document.getElementById(resultID);
+    /*create lens:*/
+    lens = document.createElement("div");
+    lens.setAttribute("class", "img-zoom-lens");
+    /*insert lens:*/
+    imageDisplayElem.parentElement.insertBefore(lens, imageDisplayElem);
+    /*calculate the ratio between result DIV and lens:*/
+    cx = result.offsetWidth / lens.offsetWidth;
+    cy = result.offsetHeight / lens.offsetHeight;
+    
+    /*set background properties for the result DIV:*/
+    /*execute a function when someone moves the cursor over the image, or the lens:*/
+    lens.addEventListener("mousemove", moveLens);
+    imageDisplayElem.addEventListener("mousemove", moveLens);
+    /*and also for touch screens:*/
+    lens.addEventListener("touchmove", moveLens);
+    imageDisplayElem.addEventListener("touchmove", moveLens);
+
+    function moveLens(e) {
+        var pos, x, y;
+        result.style.backgroundImage = "url('" + `./assets/images/Bones/visualize-bone/${selectedImage}` + "')";
+        result.style.backgroundSize = (imageDisplayElem.width * cx) + "px " + (imageDisplayElem.height * cy) + "px";
+        /*prevent any other actions that may occur when moving over the image:*/
+        e.preventDefault();
+        /*get the cursor's x and y positions:*/
+        pos = getCursorPos(e);
+        /*calculate the position of the lens:*/
+        x = pos.x - (lens.offsetWidth / 2);
+        y = pos.y - (lens.offsetHeight / 2);
+        /*prevent the lens from being positioned outside the image:*/
+        if (x > imageDisplayElem.width - lens.offsetWidth) { x = imageDisplayElem.width - lens.offsetWidth; }
+        if (x < 0) { x = 0; }
+        if (y > imageDisplayElem.height - lens.offsetHeight) { y = imageDisplayElem.height - lens.offsetHeight; }
+        if (y < 0) { y = 0; }
+        /*set the position of the lens:*/
+        lens.style.left = x + "px";
+        lens.style.top = y + "px";
+        /*display what the lens "sees":*/
+        result.style.backgroundPosition = "-" + (x * cx) + "px -" + (y * cy) + "px";
+    }
+
+    function getCursorPos(e) {
+        var a, x = 0, y = 0;
+        e = e || window.event;
+        /*get the x and y positions of the image:*/
+        a = imageDisplayElem.getBoundingClientRect();
+        /*calculate the cursor's x and y coordinates, relative to the image:*/
+        x = e.pageX - a.left;
+        y = e.pageY - a.top;
+        /*consider any page scrolling:*/
+        x = x - window.pageXOffset;
+        y = y - window.pageYOffset;
+        return { x: x, y: y };
+    }
+}
+
 // Load the file list and evaluations on page load
 window.onload = () => {
     loadFileList();
@@ -233,3 +293,7 @@ trueBtnElem.addEventListener("click", () => evaluateImage("True"));
 falseBtnElem.addEventListener("click", () => evaluateImage("False"));
 downloadBtnElem.addEventListener("click", downloadEvaluations);
 resetBtnElem.addEventListener("click", resetEvaluations);
+
+imageZoom("imageDisplay", "myresult");
+
+console.log("imageDisplayElem.src: ", imageDisplayElem.src)
